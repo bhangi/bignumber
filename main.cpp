66f2f03796fee5f16dfc88b2f1264a4b65fc0a56
bhangi <bhangi.bot@gmail.com>
2017-09-27 22:13:56 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-#include <string>
 #include <exception>
-#include <ostream>
-#include <iterator>
-#include <cctype>
-#include <algorithm>
-#include <vector>
-#include <cctype>
-#include <sstream>
-#include <deque>
-#include <ctime>
-#include <thread>
-#include <future>
-#include <functional>
-#include <fstream>
-#include <cassert>
-#include <random>
+#include <chrono>
 
 #include "bigint.hpp"
 
@@ -26,7 +11,7 @@ try{
   while(cin >> x >> y){
     auto start = std::chrono::system_clock::now();
     std::cout << "x+y = " << x+y << std::endl;
-    std::cout << "x*y = " << (x*y).size() << std::endl;
+    std::cout << "x*y = " << x*y << std::endl;
     std::cout << "x-y = " << x-y << std::endl;
     auto end = std::chrono::system_clock::now();
     std::cerr << "total time: " << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << " us" << std::endl;
